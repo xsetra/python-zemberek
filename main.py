8730440bd3dbade978a67b889815187f6e7dcad7
xsetra <abdullahcaliskan511@gmail.com>
2017-02-01 15:46:06 +0200

    jpype bilgileri eklendi

--- main.py
+++ main.py
@@ -3,4 +3,14 @@
 
 import jpype
 
+# JVM / JAR Info
+jvm_path = jpype.get_default_jvm_path()
+jar_file = "zemberek-all-0.10.0.jar"
+
 # Start JVM
+jpype.startJVM(jvm_path, "-Djava.class.path="+jar_file, "-ea")
+
+
+
+# Stop JVM
+jpype.shutdownJVM()
