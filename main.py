d966514097c747d46aa2fef0ddfe014438d961d0
xsetra <abdullahcaliskan511@gmail.com>
2017-02-01 16:20:36 +0200

    Türkçe sınıfı eklemesi yapıldı. Zemberek 2 ile çalışma başarılı.

--- main.py
+++ main.py
@@ -4,13 +4,19 @@
 import jpype
 
 # JVM / JAR Info
-jvm_path = jpype.get_default_jvm_path()
-jar_file = "zemberek-all-0.10.0.jar"
+jvm_path = "/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server/libjvm.so"
 
 # Start JVM
-jpype.startJVM(jvm_path, "-Djava.class.path="+jar_file, "-ea")
+jpype.startJVM(jvm_path, "-ea", "-Djava.class.path=/home/acaliskan/github/python-zemberek/zemberek-tum-2.0.jar")
 
+Tr = jpype.JClass("net.zemberek.tr.yapi.TurkiyeTurkcesi")
+Zemberek = jpype.JClass("net.zemberek.erisim.Zemberek")
 
+tr = Tr()
+zemberek = Zemberek(tr)
+
+yanit = zemberek.kelimeCozumle("futbolcularımız")
+print(yanit[a])
 
 # Stop JVM
 jpype.shutdownJVM()
